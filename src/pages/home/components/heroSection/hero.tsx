b5b12a07a29ad59deb93c99a8bdc6790862fb328
Rambeybello <ramadan@myitura.com>
2024-05-30 03:45:42 +0100

    few changes

--- src/pages/home/components/heroSection/hero.tsx
+++ src/pages/home/components/heroSection/hero.tsx
@@ -1,37 +1,39 @@
 export const Hero = () => {
   return (
-    <div className="bg-[#DEEFF0]">
-      <div className="container flex-col md:flex-row flex items-center justify-between gap-[3rem]">
-        <div className="md:w-[45%] w-[100%]" data-aos="fade-right">
-          <h1 className="lg:text-[40px] text-[34px] md:text-left text-center">
-            Empowering Your business with data science and AI technology{" "}
-          </h1>
-          <p className="md:my-[1rem] my-8">
-            Experience the Power of Tailored Technology Solutions and Data
-            Expertise to Propel Your Business Forward. We integrate up-to-date
-            global data, advanced analytic solution{" "}
-          </p>
-          <button className="button">Contact Us</button>
+    <>
+      <div className="bg-[#DEEFF0]">
+        <div className="container flex-col md:flex-row flex items-center justify-between gap-[3rem]">
+          <div className="md:w-[45%] w-[100%]" data-aos="fade-right">
+            <h1 className="lg:text-[40px] text-[34px] md:text-left text-center">
+              Empowering Your business with data science and AI technology{" "}
+            </h1>
+            <p className="md:my-[1rem] my-8">
+              Experience the Power of Tailored Technology Solutions and Data
+              Expertise to Propel Your Business Forward. We integrate up-to-date
+              global data, advanced analytic solution{" "}
+            </p>
+            <button className="button">Contact Us</button>
+          </div>
+
+          <div className="md:w-[40%] w-[100%]" data-aos="fade-left">
+            <img src="/assets/hero.png" alt="" className="w-full" />
+          </div>
         </div>
 
-        <div className="md:w-[40%] w-[100%]" data-aos="fade-left">
-          <img src="/assets/hero.png" alt="" className="w-full" />
+        <div
+          className="container flex-col lg:flex-row flex items-center justify-between mt-[1.5rem]"
+          data-aos="fade-down">
+          <h6 className="mb-7 lg:mb-0 text-center md:text-left">
+            Trusted by amazing companies all over the world
+          </h6>
+
+          <div className="flex flex-col md:flex-row items-center justify-between gap-[4rem]">
+            <img src="/assets/info.png" alt="" className="w-[100px]" />
+            <img src="/assets/liberty.png" alt="" className="w-[100px]" />
+            <img src="/assets/inter.png" alt="" className="w-[100px]" />
+          </div>
         </div>
       </div>
-
-      <div
-        className="container flex-col lg:flex-row flex items-center justify-between mt-[1.5rem]"
-        data-aos="fade-down">
-        <h6 className="mb-7 lg:mb-0 text-center md:text-left">
-          Trusted by amazing companies all over the world
-        </h6>
-
-        <div className="flex flex-col md:flex-row items-center justify-between gap-[4rem]">
-          <img src="/assets/info.png" alt="" className="w-[100px]" />
-          <img src="/assets/liberty.png" alt="" className="w-[100px]" />
-          <img src="/assets/inter.png" alt="" className="w-[100px]" />
-        </div>
-      </div>
-    </div>
+    </>
   );
 };
--- src/pages/home/components/navbar/navbar.tsx
+++ src/pages/home/components/navbar/navbar.tsx
@@ -20,17 +20,20 @@ export const Navbar = () => {
   return (
     <div className="bg-[#DEEFF0]">
       <div className="container relative flex flex-row items-center justify-between">
-        <h1 className="text-[20px]">CYBERNETIC</h1>
+        <div className="flex items-center gap-4">
+          <img src="/assets/logo.png" alt="" width={"100px"} />
+          {/* <h1 className="text-[20px]">CYBERNETIC BUSINESS GROUP</h1> */}
+        </div>
 
         <ul className={`flex gap-4 nav_details ${showMenu ? "show_menu" : ""}`}>
           <li>
             <a href="/">Home</a>
           </li>
           <li>
-            <a href="/about">About</a>
+            <a href="#about">About</a>
           </li>
           <li>
-            <a href="/about">Service</a>
+            <a href="#service">Our Services</a>
           </li>
         </ul>
 
